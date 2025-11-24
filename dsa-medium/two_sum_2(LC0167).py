772a9c9d89cfc0c2499614f760222a759b0fdc3f
othmanelfadil <othmanelfadil@outlook.fr>
2025-11-23 17:46:36 -0800

    added two sum ii

---
+++ dsa-medium/two_sum_2(LC0167).py
@@ -0,0 +1,56 @@
+# O(n)
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+      left,right = 0, len(numbers) - 1
+      while left < right:
+        if numbers[left] + numbers[right] > target:
+          right -= 1
+        elif numbers[left] + numbers[right] < target:
+          left += 1
+        else:
+          return [left + 1,right + 1]
+      return []
+    
+# LOOK AT THE PROBLEM NUMBER HAHAHAHA
+                                   -*=====:                                    
+                                -*+-++=+=-.:-:                                 
+                               ==-=--. .. ::-...                      .*#.     
+                              ::-=-        ..   .+             .-=-.--%*        
+                              .     .+:*- +:     .*+             .%%#.          
+                             ..  *%%*-:    :-.     .+   :+.        :            
+                           -=.  ++ .*+-.. .      =%%%%%=%%*  #%%*   +%+         
+                           +=.  . ..+******+.       ::+%+%%%+=%%+. +%+          
+                                                   :         -%#-               
+                                 .-:.      ::                                   
+     .%+                  -:      .:-      -                                    
+       .#%: . --          .        :.:    :.                                    
+          :%%#-                      ..::.                                      
+             .        .                                                         
+         -%=   -%%#. -%%=-=#%%-                                                 
+          *%= -+%@#:*%%*%%*+:                                                   
+               -%%#                                                                            
+
+                                                                                 
+                                                                                                         
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                      
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                 
