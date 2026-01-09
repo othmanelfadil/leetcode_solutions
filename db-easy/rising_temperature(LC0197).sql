39f118d95e0936ce0737038ad06202d0214244a7
othmanelfadil <othmanelfadil@outlook.fr>
2026-01-09 23:52:01 +0100

    fixed wrong solution for problem

--- db-easy/rising_temperature(LC0197).sql
+++ db-easy/rising_temperature(LC0197).sql
@@ -1,3 +1,2 @@
-SELECT email from Person
-GROUP BY email 
-HAVING COUNT(email) > 1
+SELECT w1.id FROM Weather w1,Weather w2 
+WHERE DATEDIFF(w1.RecordDate,w2.RecordDate)=1 AND w1.temperature>w2.temperature 
